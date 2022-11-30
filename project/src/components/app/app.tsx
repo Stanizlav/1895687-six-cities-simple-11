1ae91ba5cd6d54de76ebefe61f9f68107cd05486
Stanislav <stanislav.v.vasilyev@gmail.com>
2022-11-30 19:18:22 +0300

    создан и внедрен компонент-обертка-маршрутизатор для получения возможности перенаправления не только из компонентов

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import PropertyScreen from '../../pages/property-screen/property-screen';
+import browserHistory from '../../services/browser-history';
 import AppRoute from '../../types/app-route';
+import HistoryRouter from '../history-router/history-router';
 
 type AppProps = {
   defaultCardsCount: number;
@@ -13,14 +15,14 @@ type AppProps = {
 function App({ defaultCardsCount, nearPlacesCardsCount}: AppProps): JSX.Element {
 
   return(
-    <BrowserRouter>
+    <HistoryRouter history={browserHistory}>
       <Routes>
         <Route path = {AppRoute.Main} element = {<MainScreen defaultCardsCount={defaultCardsCount}/>}/>
         <Route path = {AppRoute.Login} element = {<LoginScreen/>}/>
         <Route path = {`${AppRoute.Room}/:id`} element = {<PropertyScreen cardsCount={nearPlacesCardsCount}/>}/>
         <Route path = {AppRoute.Other} element = {<NotFoundScreen/>}/>
       </Routes>
-    </BrowserRouter>
+    </HistoryRouter>
   );
 }
 
