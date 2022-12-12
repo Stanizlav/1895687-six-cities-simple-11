3d9cfa71e32a427a9009d893fcb21d3b1d664fc3
Stanislav <stanislav.v.vasilyev@gmail.com>
2022-12-12 18:18:53 +0300

    написаны тесты для взаимодействий пользователя с элементом списка городов

--- project/src/components/cities-list/city-item.test.tsx
+++ project/src/components/cities-list/city-item.test.tsx
@@ -2,26 +2,63 @@ import { render, screen } from '@testing-library/react';
 import { getRandomCity } from '../../utils/mocks';
 import CityItem from './city-item';
 import { Provider } from 'react-redux';
-import { configureMockStore } from '@jedmao/redux-mock-store';
+import { configureMockStore, MockStore } from '@jedmao/redux-mock-store';
 import HistoryRouter from '../history-router/history-router';
 import { createMemoryHistory } from 'history';
+import CitiesName from '../../types/cities-name';
+import userEvent from '@testing-library/user-event';
+import { changeCity } from '../../store/application-process/application-process';
 
 const mockStore = configureMockStore();
-const store = mockStore();
 const history = createMemoryHistory();
 
-describe('Component: CityItem', ()=>{
-  it('should render correctly', ()=>{
-    const cityName = getRandomCity().name;
+const fakeCityItem = (storage: MockStore, cityName:CitiesName, isActive = false) => (
+  <Provider store={storage}>
+    <HistoryRouter history={history}>
+      <CityItem cityName={cityName} isActive={isActive}/>
+    </HistoryRouter>
+  </Provider>
+);
 
-    render(
-      <Provider store={store}>
-        <HistoryRouter history={history}>
-          <CityItem cityName={cityName}/>
-        </HistoryRouter>
-      </Provider>
-    );
+describe('Component: CityItem', ()=>{
+  let store: MockStore;
+  let cityName: CitiesName;
+
+  beforeEach(() => {
+    store = mockStore();
+    cityName = getRandomCity().name;
+  });
+
+  it('should render correctly', ()=>{
+    render(fakeCityItem(store, cityName));
 
     expect(screen.getByText(cityName)).toBeInTheDocument();
   });
+
+  it('should dispatch the "changeCity" action when a user clicks the inactive item', async()=>{
+    render(fakeCityItem(store, cityName));
+
+    const linkElement = screen.getByRole('link');
+    expect(store.getActions()).toEqual([]);
+
+    await userEvent.click(linkElement);
+
+    expect(store.getActions()).toEqual([
+      changeCity(cityName)
+    ]);
+
+  });
+
+  it('should not dispatch the "changeCity" action when a user clicks the active item', async()=>{
+    render(fakeCityItem(store, cityName, true));
+
+    const linkElement = screen.getByRole('link');
+    expect(store.getActions()).toEqual([]);
+
+    await userEvent.click(linkElement);
+
+    expect(store.getActions()).toEqual([]);
+
+  });
+
 });
