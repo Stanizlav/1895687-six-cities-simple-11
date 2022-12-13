7c48713ff66a3b4830562d25a657f708f591efca
Stanislav <stanislav.v.vasilyev@gmail.com>
2022-12-13 16:04:40 +0300

    добавлены тесты для формы выбора сортировки

--- project/src/components/sort-form/sort-form.tsx
+++ project/src/components/sort-form/sort-form.tsx
@@ -1,4 +1,5 @@
 import { useState, memo } from 'react';
+import { SORT_FORM_ACTIVITY_CLASS, SORT_FORM_TAB_INDEX_START } from '../../consts/consts';
 import { useAppDispatch, useAppSelector } from '../../hooks/store-hooks';
 import { chooseSortType } from '../../store/application-process/application-process';
 import { getSortType } from '../../store/application-process/selectors';
@@ -9,7 +10,8 @@ function SortForm():JSX.Element{
   const sortType = useAppSelector(getSortType);
   const dispatch = useAppDispatch();
   const [isActive, setActivity] = useState(false);
-  const classList = `places__options places__options--custom${ isActive ? ' places__options--opened' : ''}`;
+  const appendedClass = isActive ? ` ${SORT_FORM_ACTIVITY_CLASS}` : '';
+  const classList = `places__options places__options--custom${appendedClass}`;
 
   const handleSortMenuClick = () => {
     setActivity((previous)=>!previous);
@@ -23,23 +25,23 @@ function SortForm():JSX.Element{
     dispatch(chooseSortType(newSortType));
   };
 
-  const sorters = Object.values(SortType);
+  const sortOptions = Object.values(SortType);
 
   return(
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0} onClick={handleSortMenuClick}>
+      <span role="button" className="places__sorting-type" tabIndex={SORT_FORM_TAB_INDEX_START} onClick={handleSortMenuClick}>
         {sortType}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
       <ul className={classList}>
-        {sorters.map((sorter, index) =>(
+        {sortOptions.map((sortOption, index) =>(
           <SortListItem
-            key={sorter}
-            tabIndex={index + 1}
-            sortType={sorter}
+            key={sortOption}
+            tabIndex={SORT_FORM_TAB_INDEX_START + index + 1}
+            sortType={sortOption}
             currentSortType={sortType}
             onClick={handleSortMenuItemClick}
           />)
--- project/src/components/sort-form/sort-list-item.test.tsx
+++ project/src/components/sort-form/sort-list-item.test.tsx
@@ -1,17 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import SortListItem from './sort-list-item';
 import SortType from '../../types/sort-type';
+import userEvent from '@testing-library/user-event';
 
 
 describe('Component: SortForm', ()=>{
+  const TAB_INDEX = 0;
+  const SORT_TYPE = SortType.Popular;
+  const ON_CLICK_CALLBACK = jest.fn();
+
+  const fakeSortListItem = (
+    <SortListItem tabIndex={TAB_INDEX} sortType={SORT_TYPE} currentSortType={SORT_TYPE} onClick={ON_CLICK_CALLBACK}/>
+  );
 
   it('should render correctly', ()=>{
-    const sortType = SortType.Popular;
-    const onClick = jest.fn();
+    render(fakeSortListItem);
 
-    render(<SortListItem tabIndex={0} sortType={sortType} currentSortType={sortType} onClick={onClick}/>);
+    expect(screen.getByText(`${SORT_TYPE}`)).toBeInTheDocument();
+  });
 
-    expect(screen.getByText(`${sortType}`)).toBeInTheDocument();
+  it('should call the obtained callback when a user clicks on the component', async()=>{
+    const LIKELY_CALLS_COUNT = 1;
+
+    render(fakeSortListItem);
+
+    await userEvent.click(screen.getByRole('listitem'));
+
+    expect(ON_CLICK_CALLBACK).nthCalledWith(LIKELY_CALLS_COUNT, SORT_TYPE);
   });
 
 });
